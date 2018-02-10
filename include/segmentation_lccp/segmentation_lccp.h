eeb7f79d8ffb0868903bd406a6b19cae54aa35a8
jontromanab.abhijit@gmail.com <jontromanab.abhijit@gmail.com>
2018-02-10 02:35:49 -0700

    first include

--- include/segmentation_lccp/segmentation_lccp.h
+++ include/segmentation_lccp/segmentation_lccp.h
@@ -101,8 +101,8 @@ public:
   // lccp parameters
   ///value of concavity_tolerance_threshold of lccp algorithm
   double concavity_tolerance_threshold;
-  ///value of smoothness_tolerance_threshold of lccp algorithm
-  double smoothness_tolerance_hreshold;
+  ///value of smoothness_threshold of lccp algorithm
+  double smoothness_threshold;
   ///value of min_segment_size of lccp algorithm
   int min_segment_size;
   ///value of use_extended_convexity of lccp algorithm
@@ -160,7 +160,7 @@ protected:
   // LCCP segmentation parameters
   ///default value of concavity_tolerance_threshold of lccp algorithm
   static const double CONCAVITY_TOLERANCE_THRESHOLD = 10;
-  ///default value of smoothness_tolerance_threshold of lccp algorithm
+  ///default value of smoothness_threshold of lccp algorithm
   static const double SMOOTHNESS_THRESHOLD = 0.1f;
   ///default value of min_segment_size of lccp algorithm
   static const int MIN_SEGMENT_SIZE = 3;
@@ -195,7 +195,7 @@ protected:
   ///value of concavity_tolerance_threshold of lccp algorithm
   double concavity_tolerance_threshold;
   ///value of smoothness_tolerance_threshold of lccp algorithm
-  double smoothness_tolerance_hreshold;
+  double smoothness_tolerance_threshold;
   ///value of min_segment_size of lccp algorithm
   int min_segment_size;
   ///value of use_extended_convexity of lccp algorithm
@@ -301,9 +301,159 @@ public:
    */
   bool segment();
 
+  /**
+   * @brief show_super_voxels shows supervoxels in the viewer
+   * @param viewer viewer on which shows the supervoxels
+   * @param show_adjacency_map shows the connection of the supervoxels
+   * @param show_super_voxel_normals shows the normals of the supervoxels
+   */
+  void show_super_voxels(boost::shared_ptr<pcl::visualization::PCLVisualizer>& viewer, bool show_adjacency_map, bool show_super_voxel_normals);
+
+  /**
+   * @brief show_super_voxels shows supervoxels in the viewer
+   * @param viewer viewer on which shows the supervoxels
+   */
+  void show_super_voxels(boost::shared_ptr<pcl::visualization::PCLVisualizer> &viewer);
+
+  /**
+   * @brief show_segmented_objects shows segmented objects
+   * @param viewer viewer on which shows the semented objects
+   */
+  void show_segmented_objects(boost::shared_ptr<pcl::visualization::PCLVisualizer>& viewer);
+
+  /**
+   * @brief show_table_plane shows the table plane
+   * @param viewer viewer on which shows the table plane
+   */
+  void show_table_plane(boost::shared_ptr<pcl::visualization::PCLVisualizer>& viewer);
+
+  /**
+   * @brief clean_viewer cleans the viewer
+   * @param viewer viewer to clean up
+   */
+  void clean_viewer(boost::shared_ptr<pcl::visualization::PCLVisualizer>& viewer);
+
+  /**
+   * @brief get_segmented_objects get detected objects
+   * @return a vector of struct Object
+   */
+  std::vector<Object> get_segmented_objects();
+
+  /**
+   * @brief get_segmented_objects get detected objects
+   * @return  a vector of point clouds
+   */
+  std::vector<PointCloud> get_segmented_objects_simple();
+
+  /**
+   * @brief print_parameters prints the parameters of the algorithm
+   */
+  void print_parameters();
+
+  /**
+   * @brief get_input_cloud returns input cloud
+   * @return pointcloud
+   */
+  CloudPtr get_input_cloud();
+
+  /**
+   * @brief get_plane_cloud returns plane cloud
+   * @return pointcloud
+   */
+  CloudPtr get_plane_cloud();
+
+  /**
+   * @brief get_labeled_voxel_cloud returns labeled voxel cloud
+   * @return PointCloud<PointXYZL>
+   */
+  PointCloudl get_labeled_voxel_cloud();
+
+  /**
+   * @brief get_supervoxel_adjacency returns supervoxel adjacency
+   */
+  std::multimap<uint32_t, uint32_t> get_supervoxel_adjacency();
+
+  /**
+   * @brief get_supervoxel_clusters returns supervoxel clusters
+   */
+  std::map<uint32_t, pcl::Supervoxel<PointT>::Ptr> get_supervoxel_clusters();
+
+  /**
+   * @brief get_normal_cloud returns normal point cloud of the supervoxels
+   * @return
+   */
+  pcl::PointCloud<pcl::PointNormal> get_normal_cloud();
+
+  /**
+   * @brief get_plane_coefficients returns table plane coefficients
+   * @return
+   */
+  pcl::ModelCoefficients get_plane_coefficients();
+
+  // setting parameters
+  ///set disable transform
+  void set_disable_transform(bool disable_transform_in);
+  ///set voxel resolution
+  void set_voxel_resolution(double voxel_resolution_in);
+  ///set seed resolution
+  void set_seed_resolution(double seed_resolution_in);
+  ///set color importance
+  void set_color_importance(double color_importance_in);
+  ///set spatial importance
+  void set_spatial_importance(double spatial_importance_in);
+  ///set normal importance
+  void set_normal_importance(double normal_importance_in);
+
+  ///set concavity_tolerance_threshold
+  void set_concavity_tolerance_threshold(double concavity_tolerance_threshold_in);
+  ///set smoothness threshold
+  void set_smoothness_threshold(double smoothness_threshold_in);
+  ///set min segment size
+  void set_min_segment_size(int min_segment_size_in);
+  ///set use extended convexity
+  void set_use_extended_convexity(bool use_extended_convexity_in);
+  ///set use sanity criterion
+  void set_use_sanity_criterion(bool use_sanity_criterion_in);
+
+  /// set zmin
+  void set_zmin(double zmin_in);
+  ///set zmax
+  void set_zmax(double zmax_in);
+  ///set th points
+  void set_th_points(int th_points_in);
 
 
+  // Getting parameters
+  ///get disable transform
+  bool get_disable_transform();
+  ///get voxel resolution
+  double get_voxel_resolution();
+  ///get seed resolution
+  double get_seed_resolution();
+  ///get color importance
+  double get_color_importance();
+  ///get spatial importance
+  double get_spatial_importance();
+  ///get normal importance
+  double get_normal_importance();
 
+  ///get concavity tolerance threshold
+  double get_concavity_tolerance_threshold();
+  ///get smoothness threshold
+  double get_smoothness_threshold();
+  ///get min segment size
+  int get_min_segment_size();
+  ///get use extended convexity
+  bool get_use_extended_convexity();
+  ///get use sanity criterion
+  bool get_use_sanity_criterion();
+
+  ///get zmin
+  double get_zmin();
+  ///get zmax
+  double get_zmax();
+  ///get th_points
+  int get_th_points();
 };
 
 
--- src/segmentation/segmentation_lccp.cpp
+++ src/segmentation/segmentation_lccp.cpp
@@ -1,1 +1,20 @@
 #include<segmentation_lccp/segmentation_lccp.h>
+
+supervoxel_parameters::supervoxel_parameters(){
+  this->disable_transform = this->DISABLE_TRANSFORM;
+  this->voxel_resolution = this->VOXEL_RESOLUTION;
+  this->seed_resolution = this->SEED_RESOLUTION;
+  this->color_importance = this->COLOR_IMPORTANCE;
+  this->spatial_importance = this->SPATIAL_IMPORTANCE;
+  this->normal_importance = this->NORMAL_IMPORTANCE;
+
+  this->concavity_tolerance_threshold = this->CONCAVITY_TOLERANCE_THRESHOLD;
+  this->smoothness_threshold = this->SMOOTHNESS_THRESHOLD;
+  this->min_segment_size = this->MIN_SEGMENT_SIZE;
+  this->use_extended_convexity = this->USE_EXTENDED_CONVEXITY;
+  this->use_sanity_criterion = this->USE_SANITY_CRITERION;
+
+  this->zmin = this->ZMIN;
+  this->zmax = this->ZMAX;
+  this->th_points = this->TH_POINTS;
+}
