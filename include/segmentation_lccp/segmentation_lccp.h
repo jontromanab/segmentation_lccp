a8e8010bda9315eb27b2b39f0b8315ce0f8461ca
jontromanab.abhijit@gmail.com <jontromanab.abhijit@gmail.com>
2018-02-09 23:00:32 -0700

    init_commit

---
+++ include/segmentation_lccp/segmentation_lccp.h
@@ -0,0 +1,7 @@
+#ifndef SEGMENTATION_LCCP_H
+#define SEGMENTATION_LCCP_H
+
+#include<pcl/point_types.h>
+#include<pcl/point_cloud.h>
+
+#endif // SEGMENTATION_LCCP_H
---
+++ src/segmentation/segmentation_lccp.cpp
@@ -0,0 +1,1 @@
+#include<segmentation_lccp/segmentation_lccp.h>
