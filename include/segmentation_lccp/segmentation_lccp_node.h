cf659764c02ea0d078754a81ac9ec4b791641424
jontromanab.abhijit@gmail.com <jontromanab.abhijit@gmail.com>
2018-02-13 00:41:34 -0700

    ros_wrapper1

---
+++ include/segmentation_lccp/segmentation_lccp_node.h
@@ -0,0 +1,22 @@
+#ifndef SEGMENTATION_LCCP_NODE_H
+#define SEGMENTATION_LCCP_NODE_H
+#include<segmentation_lccp.h>
+#include<sensor_msgs/PointCloud2.h>
+#include<segmentation_lccp/segmentation.h>
+
+/**
+ * \brief Wrapper ROS class to obtain segmented objects by a service request
+*/
+
+class LccpSegmentationAlgorithm
+{
+public:
+private:
+  ros::ServiceServer segmentation_server_;
+  bool segmentationCallback(segmentation_lccp::segmentation::Request& req,
+                            segmentation_lccp::segmentation::Response& res);
+
+
+}
+
+#endif // SEGMENTATION_LCCP_NODE_H
